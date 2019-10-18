1c389fc0ae0d6c7a22f289b857e74a0dcbf3ef02
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-17 22:16:37 -0600

    Keep idea for school routes in mind

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@
 Rails.application.routes.draw do
   root to: 'students#index'
 
-  resources :students, only: [:index, :new]
+  resources :students
 
   resources :schools do
     resources :students
   end
+  
+  # get '/schools/:school_id/students', to: 'students#index', as: :school_students
+  # get '/schools/:school_id/students/:id', to: 'students#show', as: :student
 
 end
