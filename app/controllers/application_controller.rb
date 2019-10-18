66bd347d61a78078ebd700c1a562283b6bb34271
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-17 19:01:57 -0600

    Create flash message generator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def generate_flash(resource)
+    resource.errors.messages.each do |validation, message|
+      flash[validation] = "#{validation}: #{message}"
+    end
+  end
+
 end
