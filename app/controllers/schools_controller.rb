730b1ca4454eb2d82c9fa813cbdde1309593bafc
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-17 19:59:38 -0600

    Replace flash messages with application controller methods

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -28,7 +28,7 @@ class SchoolsController < ApplicationController
 
     respond_to do |format|
       if @school.save
-        format.html { redirect_to @school, notice: 'School was successfully created.' }
+        format.html { redirect_to @school, notice: create_flash(@school) }
         format.json { render :show, status: :created, location: @school }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class SchoolsController < ApplicationController
   def update
     respond_to do |format|
       if @school.update(school_params)
-        format.html { redirect_to @school, notice: 'School was successfully updated.' }
+        format.html { redirect_to @school, notice: update_flash(@school) }
         format.json { render :show, status: :ok, location: @school }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class SchoolsController < ApplicationController
   def destroy
     @school.destroy
     respond_to do |format|
-      format.html { redirect_to schools_url, notice: 'School was successfully destroyed.' }
+      format.html { redirect_to schools_url, notice: d_flash(@school) }
       format.json { head :no_content }
     end
   end
