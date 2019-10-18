0cc8ac62e094cfefbc4082a5bb5bb16d6aa5703d
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-17 22:16:55 -0600

    Create test outline

---
+++ spec/features/schools/school_student_index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+#  we would like Students to only 
+#  be viewed and managed within the context of a particular school.
+
+RSpec.describe "School Student Index" do
+  describe "As a School Administrator" do
+    before :each do
+
+    end
+
+    it "When I visit a student index page for a specific school" do
+
+    end
+
+  end
+end
