798f16336c50d7f45cf527e9eb81dc1c7c25ab50
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-17 19:59:51 -0600

    Fix line length

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -70,6 +70,10 @@ class StudentsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def student_params
-    params.require(:student).permit(:first_name, :last_name, :school_id, :grade_id, :program_id)
+    params.require(:student).permit(:first_name,
+                                    :last_name,
+                                    :school_id,
+                                    :grade_id,
+                                    :program_id)
   end
 end
